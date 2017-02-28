f1e0eb30bebdb8b433531e9ab29a80973e877cc3
dujiahui(杜佳桧.商城技术部.前端研发部) <dujiahui@ds.gome.com.cn>
2017-02-28 11:49:14 +0800

    完成验证条件

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-index.js
+$(function(){
+    $(".check").on("click",function(){
+        var _that = $(this);
+        var val = _that.prev().val();
+        if(!val){
+            _that.next().text("姓名不能为空");
+        }else if(val.length<4||val.length>16){ 
+            _that.next().text("必填，长度为4~16个字符");
+        }else{
+            _that.next().text("格式正确");
+        }
+    })
+})
